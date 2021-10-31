117dab08e38b16ce6a0546054aaa28f55ac3eb0b
Gonzalo Bustamante <gonzalo.b1989@hotmail.com>
2021-10-31 21:34:46 +0100

    make it better

--- public/chat.js
+++ public/chat.js
@@ -2,19 +2,29 @@
 let socket = io();
 
 // DOM elements
+let chatWindow= document.getElementById('chatWindow');
+let form = document.getElementById('form');
 let message = document.getElementById('message');
 let username = document.getElementById('username');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
-btn.addEventListener('click', function()
+form.addEventListener('submit', function(e)
 {
-  socket.emit('chat:message', {
-    message: message.value,
-    username: username.value
-  });
-  message.value='';
+  e.preventDefault();
+  if (message.value)
+  {
+    socket.emit
+    (
+      'chat:message',
+      {
+        message: message.value,
+        username: username.value
+      }
+    );
+    message.value = '';
+  }
 });
 
 message.addEventListener('keypress', function () {
@@ -23,11 +33,13 @@ message.addEventListener('keypress', function () {
 
 socket.on('chat:message', function(data) {
   actions.innerHTML = '';
-  output.innerHTML += `<p>
-    <strong>${data.username}</strong>: ${data.message}
-  </p>`
+  output.innerHTML += `<p><strong>${data.username}</strong>: ${data.message}</p>`;
+  console.log('Este es el ultimo mensaje del output: '+output.lastChild.innerHTML);
+  //Vamos al pixel establecido(x,y), en caso de que el valor establecido sea mayor que la altura o anchura va al último
+  chatWindow.scrollTo( 0, 100000000 );
 });
 
 socket.on('chat:typing', function(data) {
-  actions.innerHTML =  `<p><em>${data} is typing a message...</em></p>`
+  actions.innerHTML =  `<p><em>${data} está escribiendo un mensaje...</em></p>`;
+  chatWindow.scrollTo( 0, 100000000 );
 });
