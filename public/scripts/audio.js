a6674a87a050b6cd7251a6e43c43071a33f172d3
bustamanter18 <gonzalo.b1989@hotmail.com>
2023-07-11 20:46:01 +0200

    botones actualizados

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -1,4 +1,24 @@
 audio= document.getElementById('audio');
 
-
+function reproducir()
+{
+	audio= document.getElementById('audio');
+	audio.play();
+}
+function pausar()
+{
+	audio.pause();
+	audio.currentTime = 0;
+	audio=null
+}
+function ajustarVolumen()
+{
+	var volumenControl = document.getElementById("volumenControl");
+	audio.volume = volumenControl.value;
+}
+function mutear()
+{
+	volumenControl.value=0;
+	audio.volume = volumenControl.value;
+}
 
